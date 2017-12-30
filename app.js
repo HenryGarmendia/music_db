d43241b7e50e47bbcc1cdd2d6d1405268583f022
HenryGarmendia <me@henrygarmendia.com>
2017-12-30 05:46:07 -0600

    modified the app.js and add some functionality

---
+++ app.js
@@ -0,0 +1,63 @@
+(function musicDB() {
+    this.init = function() {
+        this.search();
+    };// init END
+
+    this.search = function() {
+        var form = document.querySelector('#form');
+
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+
+            var input_search_value = document.querySelector('#input_search').value;
+            form.reset();
+
+            get_data(input_search_value.split(' ').join('+'));
+        });
+    };// search END
+
+    this.get_data = function(artist) {
+        var http = new XMLHttpRequest();
+        var url = `https://itunes.apple.com/search?term=${artist}&entity=album`;
+        var method = 'GET';
+
+        var album_list_container = document.querySelector('#album_list');
+        album_list_container.innerHTML = '';
+
+        http.open(method, url);
+        http.onreadystatechange = function() {
+            if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+                show_artist(JSON.parse(http.response));
+            } else if (http.readyState === XMLHttpRequest.DONE && http.status !== 200) {
+                
+            }
+        }
+        http.send();
+    };// get_data END
+
+    this.show_artist = function(obj) {
+        var album_list_container = document.querySelector('#album_list');
+        var output = '';
+
+        if (obj.results.length > 0) {
+            for (let i = 0; i < obj.results.length; i++) {
+                output += '<div class="col-3 album_item">';
+                output +=   '<div class="item_tn" style="background: url();"></div>';
+                output +=   '<div class="item_title">I can\'t destroy</div>';
+                output +=   '<div class="item_price">';
+                output +=       '<span>Price:</span> 200 USD';
+                output +=   '</div>';
+                output +=   '<a href="#" target="_blank">Buy now</a>';
+                output += '</div>';
+            }
+            album_list_container = '';
+            album_list_container.insertAdjacentHTML('afterbegin', output);
+        } else {
+            // display errors
+            document.querySelector('#no_match').style.display = 'block';
+        }
+
+    };// show_artist END
+    
+    this.init();
+})();
